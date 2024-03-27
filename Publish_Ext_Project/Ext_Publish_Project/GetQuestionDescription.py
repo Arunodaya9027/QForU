aa0dbeb1aff984eadd0805882b45c6fc5c3d06ed
Arunodaya9027 <arunodaya9027p.sg@gmail.com>
2024-03-27 18:22:48 +0530

    Final Version 1

---
+++ Publish_Ext_Project/Ext_Publish_Project/GetQuestionDescription.py
@@ -0,0 +1,212 @@
+
+from bs4 import BeautifulSoup
+import requests
+import time
+import json
+import mysql.connector
+from mysql.connector import Error
+from tqdm import tqdm
+
+
+class GetQuestionDescription:
+    """A class to scrape the description of all questions.
+
+    Args:
+        limit (int, optional): The maximum number of questions to query for from Leetcode's graphql API. 
+                               Defaults to 10,000.
+    """
+
+    # Specify the path to the txt file
+    txt_path = "_db_config.txt"
+
+    # Read the txt file
+    with open(txt_path, "r") as file:
+        content = file.read()
+
+    # Parse the JSON content
+    db_config = json.loads(content)
+
+    def __init__(self, limit: int = 10000):
+        self.limit = limit
+        self.questions = None
+
+    def scrape(self):
+        """Scrapes LeetCode data of questions body.
+        """
+
+        self._get_question()
+
+    def _get_question(self):
+        print("Scraping question description...", end="")
+        # getting the url list from the database
+        url_list = GetQuestionDescription.db_to_data(
+            GetQuestionDescription.db_config)
+        for url in enumerate(tqdm(url_list)):
+            questions_list = []     # list to store the questions description
+            links = url[1][0]       # getting the url from the list
+            r = requests.get(links)  # sending request to the url
+            html_text = r.text  # getting the html text
+            # parsing the html text using lxml parser
+            soup = BeautifulSoup(html_text, 'lxml')
+            # finding the div tag which is containg main material of the Question
+            question = soup.find(
+                'div', class_='flex h-full w-full flex-1 flex-col')
+
+            # if the question is not found because of the high traffic or loading issue
+            if question is None:        
+                continue
+            else:
+                # description of the question
+                description_beautify = question.find('div', class_='_1l1MA')
+
+                # Remove all <img> tags
+                for img in description_beautify.find_all('img'):
+                    img.decompose()
+
+                # Remove all 'href' attributes
+                for a in description_beautify.find_all('a'):
+                    del a['href']
+
+                # Convert the html content in description to a string
+                description = str(description_beautify)
+                # Count the total number of <pre> tags
+                total_pre = description.count("<pre>")
+                temp_pre_ends = 0   # Variable to store the temporary end index of the <pre> tag
+
+                time.sleep(1)
+
+                for i in range(total_pre):
+                    # Find the start index of the <pre> tag
+                    pre_index_starts = description.find("<pre>", temp_pre_ends)
+                    pre_index_ends = description.find(
+                        "</pre>", pre_index_starts)     # Find the end index of the <pre> tag
+                    description = description[:pre_index_starts] + \
+                        "<div style='background-color:#000a2050; padding: 2px 0px 2px 20px'>" + \
+                        description[pre_index_starts:pre_index_ends] + \
+                        "</div>" + \
+                        description[pre_index_ends:]     # Add a <div> tag around each <pre> tag
+                    temp_pre_ends = description.find(
+                        "</pre>", pre_index_starts)     # Update the end index for next interation
+
+                # Add the styling to the <code> tag
+                description = description.replace(
+                    "<code>", "<code style='background-color:#000a2050; max-width:fit-content; padding: 3px 5px 3px 5px'>")
+
+                # Add the orignal styling to the <li> tag
+                description = description.replace(
+                    "<li>", "<li style='margin-bottom:5px'>")
+
+                # Add a line break before each <strong> tag
+                description = description.replace("\n<strong>", "<br><strong>")
+
+                # Add the original styling of the <strong> tag
+                description = description.replace(
+                    "<strong>", "<strong style='margin=10 0 10 0'>")
+                # Remove all unnecessary line breaks
+                description = description.replace("\n", "")
+
+                # Update the extracted style attribute to get question in its original format
+                description = description.replace(
+                    'style="width:253px;height:253px"', 'style="width:253px;height:253px; margin: 0 0 15px 0"')
+
+                # Create a BeautifulSoup object
+                soup = BeautifulSoup(description, "html.parser")
+
+                # Find and remove the specific code blocks
+                code_blocks = soup.find_all(
+                    "div", style="background-color:#000a2050; padding: 2px 0px 2px 20px")
+                for code_block in code_blocks:
+                    if code_block is not None:
+                        code_block.extract()
+
+                # Remove the example sections
+                examples = soup.find_all("strong", class_="example")
+                for example in examples:
+
+                    if example.parent is not None:
+                        example.parent.extract()  # Remove the parent element containing the example
+
+                    if example.parent.next_sibling:
+                        example.parent.next_sibling.extract()  # Remove the associated <div> element
+
+                # Print the modified HTML
+                description = str(soup.prettify())
+
+                # Add a line break before each <strong> tag
+                description = description.replace("\n<strong>", "<br><strong>")
+                # Remove all unnecessary line breaks
+                description = description.replace("\n", "")
+                description = description.replace(
+                    "</code>", "</code>&nbsp;")  # Add a non-breaking space after each </code> tag
+
+                questions_list.append({"Link": links, "Question": description})
+
+                GetQuestionDescription.data_to_db(
+                    self, questions_list, GetQuestionDescription.db_config)
+
+        print("Done")
+
+    def db_to_data(db_config: dict) -> list:
+        # connecting to the database using the connect() method
+        connection = mysql.connector.connect(**db_config)
+        cursor = connection.cursor()    # creating a cursor object using the cursor() method
+        try:
+            if connection.is_connected():       # Checking if the connection is established
+                # Executing SQL Queries using the execute() method
+                cursor = connection.cursor()
+
+                # Executing SQL Queries using the execute() method
+                sql = "SELECT OriginalLink FROM question_master_coding WHERE Question IS NULL"
+                # Fetch all the rows using the fetchall() method
+                cursor.execute(sql)
+                val = cursor.fetchall()     # Printing the result one by one
+                connection.commit()     # Commiting the changes
+                return val
+        except Error as e:
+            # Handling the connection error
+            print("Error while connecting to MySQL\n\n", e)
+        finally:
+            if connection.is_connected():
+                cursor.close()      # Closing the cursor object
+                connection.close()      # Closing the connection
+                print("MySQL connection is closed\n\n")
+
+    def data_to_db(self, question, db_config: dict):
+        # connecting to the database using the connect() method
+        connection = mysql.connector.connect(**db_config)
+        # creating a cursor object using the cursor() method
+        cursor = connection.cursor()
+        try:
+            if connection.is_connected():
+                # creating a cursor object using the cursor() method
+                cursor = connection.cursor()
+
+                if question not in [None, []]:
+                    # Preparing SQL query to INSERT a record into the database.
+                    question_sql = "UPDATE question_master_coding SET Question = %s WHERE OriginalLink = %s"
+                    # Preparing a list of values to be inserted into the database.
+                    values = [question[0]["Question"], question[0]
+                              ["Link"]]     
+                    # Executing the SQL command
+                    cursor.execute(question_sql, values)
+
+                    # commiting the connection then closing it.
+                    connection.commit()
+                    # sleep for 2 seconds 
+                    # once the data is inserted so that the database is not overloaded 
+                    # with multiple requests
+                    time.sleep(2)
+                else:
+                    # calling the get_question() function
+                    question = GetQuestionDescription.get_question()
+
+        except Error as e:
+            # Printing the link of the question where the error occured
+            print(question[0]["Link"])
+            # Handling the connection error
+            print("Error while connecting to MySQL", e)
+        finally:
+            if connection.is_connected():
+                cursor.close()      # Closing the cursor object
+                connection.close()      # Closing the connection
+
---
+++ Publish_Ext_Project/Ext_Publish_Project/launch.py
@@ -0,0 +1,190 @@
+
+from GetQuestionsList import GetQuestionsList
+from GetTestcaseList import GetTestcaseList
+from GetExampleList import GetExampleList
+from GetQuestionDescription import GetQuestionDescription
+import mysql.connector
+from mysql.connector import Error
+import json
+
+
+class launch_scraper:
+    # Specify the path to the txt file
+    txt_path = "_db_config.txt"
+
+    # Read the txt file
+    with open(txt_path, "r") as file:
+        content = file.read()
+
+    # Parse the JSON content
+    db_config = json.loads(content)
+
+    def __init__(self):
+        print("Launching scraper...")
+        self._get_db(launch_scraper.db_config)
+
+    def _get_db(self, db_config):
+        connection = mysql.connector.connect(**db_config)
+        cursor = connection.cursor()
+        try:
+            if connection.is_connected():
+                db_Info = connection.get_server_info()
+                print("Connected to MySQL Server version ", db_Info)
+                cursor = connection.cursor()
+                cursor.execute("select database();")
+                record = cursor.fetchone()
+                print("You're connected to database: ", record)
+
+                tables_all = "SHOW TABLES;"
+                flag_master_table, flag_testcase_table, flag_example_table = False, False, False
+                cursor.execute(tables_all)
+                tables = cursor.fetchall()
+                for table in tables:
+                    if table[0] == "question_master_coding":
+                        flag_master_table = True
+                    elif table[0] == "question_master_testcase_coding":
+                        flag_testcase_table = True
+                    elif table[0] == "question_master_detail_coding":
+                        flag_example_table = True
+
+                if flag_master_table == False and flag_testcase_table == False and flag_example_table == False:
+                    self._create_table(db_config)
+                    print("Tables created successfully")
+
+                elif flag_master_table == True and flag_testcase_table == True and flag_example_table == True:
+                    print("Connecting Tables from Database to Scraper ...")
+
+                elif flag_master_table == True or flag_testcase_table == True or flag_example_table == True:
+                    print("Mismatching of Tables Found")
+                    print("Some tables exist, some don't. Please check the database")
+
+        except Error as e:
+            print("Error while connecting to MySQL", e)
+            print()
+
+        finally:
+            if connection.is_connected():
+                cursor.close()
+                connection.close()
+                # print("MySQL connection is closed")
+
+    def _create_table(self, db_config):
+        connection = mysql.connector.connect(**db_config)
+        cursor = connection.cursor()
+        try:
+            if connection.is_connected():
+                cursor = connection.cursor()
+                key = "SET FOREIGN_KEY_CHECKS=0;"
+
+                # Table structure for `question_master_testcase_coding`
+                sql = """CREATE TABLE `question_master_testcase_coding` (
+                    `id` bigint NOT NULL AUTO_INCREMENT,
+                    `Question_Id` bigint DEFAULT NULL,
+                    `TestCaseNo` int DEFAULT NULL,
+                    `Custom_Input` longtext,
+                    `Custom_Output` longtext,
+                    `Explanation` longtext CHARACTER SET latin1 COLLATE latin1_swedish_ci,
+                    PRIMARY KEY (`id`)
+                    ) ENGINE=InnoDB AUTO_INCREMENT=5138 DEFAULT CHARSET=latin1;"""
+                cursor.execute(key)
+                cursor.execute(sql)
+                connection.commit()
+
+                key = "SET FOREIGN_KEY_CHECKS=0;"
+
+                # Table structure for `question_master_coding_temp`
+                sql = """CREATE TABLE `question_master_coding` (
+                    `Question_ID` bigint NOT NULL AUTO_INCREMENT,
+                    `Session` varchar(255) DEFAULT NULL,
+                    `Subject_Code` varchar(255) DEFAULT NULL,
+                    `Topic` longtext CHARACTER SET latin1 COLLATE latin1_swedish_ci,
+                    `TopicID` bigint DEFAULT NULL,
+                    `Title` longtext CHARACTER SET latin1 COLLATE latin1_swedish_ci,
+                    `Question` longtext CHARACTER SET latin1 COLLATE latin1_swedish_ci,
+                    `QuestionLevel` varchar(255) DEFAULT NULL,
+                    `OriginalLink` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL,
+                    `Status` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT '' COMMENT 'ACTIVE',
+                    `Uploaded_By` varchar(255) DEFAULT NULL,
+                    `Uploaded_On` datetime DEFAULT NULL,
+                    `Uploaded_From` varchar(255) DEFAULT NULL,
+                    `DeletedBy` varchar(255) DEFAULT NULL,
+                    `DeletedOn` datetime DEFAULT NULL,
+                    `DeletedFrom` varchar(255) DEFAULT NULL,
+                    `DeletedRemark` varchar(255) DEFAULT NULL,
+                    `QuestionMark` int DEFAULT '1',
+                    `QuestionMode` varchar(255) DEFAULT 'Coding Problem',
+                    `QuestionType` varchar(255) DEFAULT 'Academic',
+                    `QuestionFor` varchar(255) DEFAULT 'Main',
+                    `ExpectedAnswer` longtext,
+                    `VerifyBy` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL,
+                    `VerifyOn` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL,
+                    `VerifyFrom` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL,
+                    PRIMARY KEY (`Question_ID`),
+                    KEY `a` (`Subject_Code`) USING BTREE,
+                    KEY `b` (`Session`) USING BTREE,
+                    KEY `c` (`Question_ID`) USING BTREE,
+                    KEY `d` (`Status`) USING BTREE,
+                    KEY `e` (`TopicID`) USING BTREE,
+                    KEY `f` (`QuestionLevel`) USING BTREE,
+                    KEY `g` (`Status`) USING BTREE,
+                    KEY `i` (`QuestionMark`) USING BTREE,
+                    KEY `j` (`QuestionMode`) USING BTREE,
+                    KEY `k` (`QuestionType`) USING BTREE
+                    ) ENGINE=InnoDB AUTO_INCREMENT=2254 DEFAULT CHARSET=latin1;
+
+                    """
+                cursor.execute(key)
+                cursor.execute(sql)
+                connection.commit()
+
+                key = "SET FOREIGN_KEY_CHECKS=0;"
+
+                # Table structure for `question_master_detail_coding_temp`
+                sql = """CREATE TABLE `question_master_detail_coding` (
+                    `id` bigint NOT NULL AUTO_INCREMENT,
+                    `Question_Id` bigint DEFAULT NULL,
+                    `ExampleNo` int DEFAULT NULL,
+                    `Input` longtext,
+                    `ExpectedOutput` longtext,
+                    `Explanation` longtext,
+                    PRIMARY KEY (`id`)
+                    ) ENGINE=InnoDB AUTO_INCREMENT=8373 DEFAULT CHARSET=latin1;"""
+                cursor.execute(key)
+                cursor.execute(sql)
+                connection.commit()
+
+        except Error as e:
+            print("Error while connecting to MySQL", e)
+            print()
+
+        finally:
+            if connection.is_connected():
+                cursor.close()
+                connection.close()
+
+    def question_list_scrape(self):
+        ls = GetQuestionsList()
+        ls.scrape()  # Scrape the list of questions
+        ls.to_sql(ls.db_config)  # Save the list of questions to a SQL database
+
+    def get_description(self):
+        gd = GetQuestionDescription()
+        gd.scrape()  # Scrape the description of all questions
+
+    def test_scrape(self):
+        ts = GetTestcaseList()
+        ts.scrape()  # Scrape the list of testcases of all questions
+
+    def example_scrape(self):
+        es = GetExampleList()
+        es.scrape()  # Scrape the list of examples of all questions
+
+
+
+    def start(self):
+        # Call the function to launch
+        self.question_list_scrape()
+        self.get_description()
+        self.test_scrape()
+        self.example_scrape()
+
